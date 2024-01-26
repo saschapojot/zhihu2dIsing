a95356c640851105e96b775c9fe5daf07cba7885
Xi LIU <xi.liu@u.nus.edu>
2024-01-26 14:24:38 +0800

    first commit

---
+++ computeLag.py
@@ -0,0 +1,82 @@
+import pickle
+import numpy as np
+import random
+from copy import deepcopy
+from datetime import datetime
+
+random.seed(198)
+
+N = 30  # length of one direction
+J = 1
+
+T = 0.1
+beta = 1 / T
+
+# init s
+tInitStart = datetime.now()
+sVals = [-1, 1]
+sCurr = np.zeros((N, N), dtype=float)
+for i in range(0, N):
+    for j in range(0, N):
+        sCurr[i, j] = sVals[random.randint(0, 1)]
+
+tInitEnd = datetime.now()
+print("init time: ", tInitEnd - tInitStart)
+tMCStart = datetime.now()
+flipNum = 0
+notFlipNum = 0
+maxStep = 100 * N ** 2
+
+
+class computationData:  # holding computational results to be dumped using pickle
+    def __init__(self):
+        self.sAll = []  # list
+
+
+record = computationData()
+for tau in range(0, maxStep):
+    print("step " + str(tau))
+    tOneMCStepStart = datetime.now()
+
+    # flip s
+    a = random.randint(0, N - 1)
+    b = random.randint(0, N - 1)
+    sLeft = sCurr[a, (b - 1) % N]
+    sRight = sCurr[a, (b + 1) % N]
+    sUp = sCurr[(a - 1) % N, b]
+    sDown = sCurr[(a + 1) % N, b]
+
+    DeltaE = 2 * J * sCurr[a, b] * (sLeft + sRight + sUp + sDown)
+    print("Delta E=" + str(DeltaE))
+    if DeltaE <= 0:
+        print("Delta E<=0")
+        sCurr[a, b] *= -1
+        print("flipped")
+        flipNum += 1
+    else:
+        r = random.random()
+        print("r=" + str(r))
+        print("exp(-beta*Delta E)=" + str(np.exp(-beta * DeltaE)))
+        if r < np.exp(-beta * DeltaE):
+            sCurr[a, b] *= -1
+            print("flipped")
+            flipNum += 1
+        else:
+            print("not flipped")
+            notFlipNum += 1
+    record.sAll.append(sCurr)
+    print("sCurr=" + str(sCurr))
+    tOneMCStepEnd = datetime.now()
+    print("one step MC :", tOneMCStepEnd - tOneMCStepStart)
+    print("=====================================")
+    if tau % 5000 == 0:
+        print("flip " + str(tau))
+
+tMCEnd = datetime.now()
+print("MC time: ", tMCEnd - tMCStart)
+print("flip num: " + str(flipNum))
+print("no flip num: " + str(notFlipNum))
+
+outPklFileName = "T" + str(T) + "step" + str(maxStep) + "out.pkl"
+with open(outPklFileName, "wb") as fptr:
+    pickle.dump(record, fptr, pickle.HIGHEST_PROTOCOL)
